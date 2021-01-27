6ddf2f2053bd7178192469f18b2da3033a18b670
LizardKingAce <lizardkingace@gmail.com>
2021-01-27 13:38:28 -0700

    Added simple plane, player sphere, simple player controller script, and camera controller script
    Initial project set up. Camera controller script contains commented out code. Might come back to it later to add a way for the camera to follow the ball on all axises while still allowing the sphere to roll. Otherwise, will either keep it this way with following the player's transform at an offest, or will simply parent the camera to the player and add constraints to lock the rotation in the inspector. Will come back to this script.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
+
+/*public GameObject player;
+private Vector3 offset;
+
+float distance;
+Vector3 playerInitialPosition;
+Vector3 playerDirecton;
+
+// Start is called before the first frame update
+void Start()
+{
+    offset = transform.position - player.transform.position;
+
+    distance = offset.magnitude;
+    playerInitialPosition = player.transform.position;
+}
+
+// Update is called once per frame
+void LateUpdate()
+{
+    //transform.position = player.transform.position + offset;
+    playerDirecton = player.transform.position - playerInitialPosition;
+    if (playerDirecton != Vector3.zero)
+    {
+        float yy = transform.position.y;
+        yy = 4f;
+
+        playerDirecton.Normalize();
+        transform.position = player.transform.position - playerDirecton * distance;
+
+        transform.position = new Vector3(transform.position.x, yy, transform.position.z);
+
+        transform.LookAt(player.transform.position);
+
+        playerInitialPosition = player.transform.position;
+    }
+} */
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hor, 0f, ver);
+
+        rb.AddForce(movement * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
