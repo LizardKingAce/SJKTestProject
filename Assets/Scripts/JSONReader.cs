8d3beb489f37ac3bf323e5d59b55e9e07e9ba35a
LizardKingAce <lizardkingace@gmail.com>
2021-01-29 18:02:29 -0700

    JSON added( PrismJSON.json)! Prism Prefabs made! New scripts JSONReader (really should rename this), PrismData, Prisms added
    JSON is neat. Added a bunch of scripts. Prisms don't move yet and have no direction yet, but each prefab has a set speed and color gained from the JSON file. Next I'll get the prisms to move and give them they're own special behaviors. From there, I'll go back to work on the player with the lives system. That'll be the goals for tomorrow. Reminder: Maybe commit more often?

---
+++ Assets/Scripts/JSONReader.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public PrismData pd = new PrismData();
+
+    void Start()
+    {
+        
+
+        Prisms prismsInJson = JsonUtility.FromJson<Prisms>(jsonFile.text);
+
+        foreach (PrismData prism in prismsInJson.prisms)
+        {
+            if(prism.prismID == "Red Prism")
+            {
+                pd.prismID = prism.prismID;
+                pd.speed = prism.speed;
+                pd.r = prism.r;
+                pd.g = prism.g;
+                pd.b = prism.b;
+                pd.a = prism.a;
+
+                if(this.gameObject.CompareTag("Red Prism"))
+                {
+                    gameObject.GetComponent<Renderer>().material.color = new Color(pd.r, pd.g, pd.b, pd.a);
+                    return;
+                }              
+            }
+
+            if (prism.prismID == "Green Prism")
+            {
+                pd.prismID = prism.prismID;
+                pd.speed = prism.speed;
+                pd.r = prism.r;
+                pd.g = prism.g;
+                pd.b = prism.b;
+                pd.a = prism.a;
+
+                if (this.gameObject.CompareTag("Green Prism"))
+                {
+                    gameObject.GetComponent<Renderer>().material.color = new Color(pd.r, pd.g, pd.b, pd.a);
+                    return;
+                }
+            }
+
+            if (prism.prismID == "Purple Prism")
+            {
+                pd.prismID = prism.prismID;
+                pd.speed = prism.speed;
+                pd.r = prism.r;
+                pd.g = prism.g;
+                pd.b = prism.b;
+                pd.a = prism.a;
+
+                if (this.gameObject.CompareTag("Purple Prism"))
+                {
+                    gameObject.GetComponent<Renderer>().material.color = new Color(pd.r, pd.g, pd.b, pd.a);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PrismData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrismData
+{
+    public string prismID;
+    public float speed;
+    //public Vector3 direction;
+
+    //color
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+}
+
